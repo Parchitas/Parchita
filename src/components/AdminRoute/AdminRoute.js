869f2efaf606ee24f0d6283df3231604b7ef04a6
Jose Rubin <jmanuelrubin1@gmail.com>
2022-03-20 02:57:01 -0400

    Listo pagina de usuario y su boton con rutas privadas

--- src/components/AdminRoute/AdminRoute.js
+++ src/components/AdminRoute/AdminRoute.js
@@ -1,1 +1,17 @@
 import React from "react";
+import React, { useContext } from "react";
+import {sessionContext} from "../../context/SessionContext"
+import { Navigate, useLocation } from "react-router-dom";
+
+export default function AdminRoute({children}){
+
+    const {isAdmin} = useContext(sessionContext);
+    const location = useLocation();
+    if (!isAdmin) {
+        console.log(location)
+        return <Navigate to="/login?fromPrivateRoute=true" state={{ from: location }} replace/>
+    }
+
+    return children
+
+}  
