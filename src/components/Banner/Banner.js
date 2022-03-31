4e68a01c8675628957b43727f17c0a19caf2097d
royvicente90 <r.roy@correo.unimet.edu.ve>
2022-03-31 05:05:25 -0400

    Fixing last things

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -12,35 +12,47 @@ const Banner = () => {
   const {isAdmin} = useContext(sessionContext)
   const classes=useStyle()
   return (
-    
-    <div className={classes.root }>
+    <div className={classes.root}>
       {/*<div className={classes.info }>
         <Typography variant="h2"> Have Fun</Typography>
         <Button variant="contained">Check our Rooms</Button>
       </div>
-      */}<br/>
+      */}
+      <br />
+      <br />
+      <br />
       <div>
-        <Grid container justifyContent="center" alignItems="center" spacing={5}><br />
-          <Grid item><Box
-                    sx={{
-                        width: 400,
-                        height: 300,
-                    }}
-                ><Card>
-          <CardContent>
-            <Typography variant="h5" align="center">¿Que es Parchita?</Typography><br />
-      <Typography variant="body1" align="justify">Sabemos que cuando visitas un hotel puedes llevarte una experiencia tanto ácida como dulce ¡¡¡COMO LA PARCHITA!!!.<br/><br/> Por eso mismo ponemos a su disposición una plataforma donde podrás obtener información útil de las ciudades listadas, así como sitios de interés y te ofrecemos también la posibilidad de reservar una habitación en uno de los grandes hoteles con los que poseemos contacto, para que disfrutes de ese viaje que tanto desean.</Typography>
-        </CardContent>
-            </Card></Box></Grid>
-        <Grid item sx>    
-                    <Button component={Link} to="/ciudades" size="large" variant='contained' color='error' >Explora tus ciudades</Button><br/><br/>
-          {isAdmin ?
-          
-                    <Button component={Link} to="/dashboard" size="large" variant='contained' color='error' >Editar Base de Datos</Button>
-          : null}</Grid></Grid>
+        <Grid container justifyContent="center" alignItems="center" spacing={5}>
+          <br />
+
+          <Grid item sx>
+            <Button
+              component={Link}
+              to="/ciudades"
+              size="large"
+              variant="contained"
+              color="error"
+            >
+              Explora tus ciudades
+            </Button>
+            <br />
+            <br />
+            {isAdmin ? (
+              <Button
+                component={Link}
+                to="/dashboard"
+                size="large"
+                variant="contained"
+                color="error"
+              >
+                Editar Base de Datos
+              </Button>
+            ) : null}
+          </Grid>
+        </Grid>
       </div>
     </div>
-  )
+  );
 }
 
 
--- src/pages/DashboardEditHotelPage.js
+++ src/pages/DashboardEditHotelPage.js
@@ -1,163 +1,243 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { queryHotel, updateHoteles } from "../services/hoteles"
+import { queryHotel, updateHoteles } from "../services/hoteles";
 import { queryHabitacion } from "../services/habitaciones";
-import { Grid, Card, CardContent, TextField, Button, Typography, } from "@material-ui/core";
+import {
+  Grid,
+  Card,
+  CardContent,
+  TextField,
+  Button,
+  Typography,
+} from "@material-ui/core";
 import { Form } from "react-bootstrap";
 import { AddCircle, Block } from "@material-ui/icons";
-import { CircularProgress } from "@material-ui/core"
-
+import { CircularProgress } from "@material-ui/core";
 
 function DashboardEditHotelPage() {
+  const navigate = useNavigate();
+  const { hotelID } = useParams();
+  const [hotel, setHotel] = React.useState();
+  const [loading, setLoading] = React.useState(true);
+  const [instalacionesInput, setInstalacionesInput] = React.useState("");
+  const [tipoHabitacionesInput, setTipoHabitacionesInput] = React.useState("");
 
-    const navigate = useNavigate()
-    const { hotelID } = useParams();
-    const [hotel, setHotel] = React.useState()
-    const [loading, setLoading] = React.useState(true)
-    const [instalacionesInput, setInstalacionesInput] = React.useState("");
-    const [tipoHabitacionesInput, setTipoHabitacionesInput] = React.useState("");
+  React.useEffect(() => {
+    setLoading(true);
+    queryHotel(hotelID).then((response) => {
+      setHotel(response);
+      setLoading(false);
+    });
+  }, []);
 
-    React.useEffect(() => {
+  function onChange(e) {
+    const formName = e.target.name;
+    const formValue = e.target.value;
 
-        setLoading(true)
-        queryHotel(hotelID).then((response) => {
-            setHotel(response)
-            setLoading(false)
-        })
+    setHotel((prevHotel) => ({ ...prevHotel, [formName]: formValue }));
+  }
 
-    }, [])
+  function addTipoHabitaciones() {
+    setHotel({
+      ...hotel,
+      tipoHabitaciones: hotel.tipoHabitaciones.concat(tipoHabitacionesInput),
+    });
+    setTipoHabitacionesInput("");
+  }
 
-    function onChange(e) {
-        const formName = e.target.name;
-        const formValue = e.target.value;
+  function deleteTipoHabitacion(idx) {
+    setHotel({
+      ...hotel,
+      tipoHabitaciones: hotel.tipoHabitaciones.filter(
+        (tipoHabitaciones, index) => idx !== index
+      ),
+    });
+  }
 
-        setHotel(prevHotel => ({ ...prevHotel, [formName]: formValue }));
+  function deleteInstalacion(idx) {
+    setHotel({
+      ...hotel,
+      instalaciones: hotel.instalaciones.filter(
+        (instalaciones, index) => idx !== index
+      ),
+    });
+  }
+
+  function addInstalaciones() {
+    setHotel({
+      ...hotel,
+      instalaciones: hotel.instalaciones.concat(instalacionesInput),
+    });
+    setInstalacionesInput("");
+  }
+
+  function validarArrays() {
+    if (instalacionesInput.length === 0) {
+      return false;
+    } else {
+      return true;
     }
+  }
 
-    function addTipoHabitaciones() {
-        setHotel({ ...hotel, tipoHabitaciones: hotel.tipoHabitaciones.concat(tipoHabitacionesInput) })
-        setTipoHabitacionesInput("");
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function deleteTipoHabitacion(idx) {
-        setHotel({ ...hotel, tipoHabitaciones: hotel.tipoHabitaciones.filter((tipoHabitaciones, index) => idx !== index) })
-    }
+    updateHoteles(hotel, hotelID, hotel.ciudadID)
+      .then(() => {
+        navigate("/dashboardHoteles");
+      })
+      .catch((e) => console.error({ error: e, msg: "ta malardo" }));
+  }
 
+  const getNombreHab = async (id) => {
+    return await queryHabitacion(id);
+  };
 
-    function deleteInstalacion(idx) {
-        setHotel({ ...hotel, instalaciones: hotel.instalaciones.filter((instalaciones, index) => idx !== index) })
-    }
+  if (loading) return <CircularProgress />;
 
-    function addInstalaciones() {
-        setHotel({ ...hotel, instalaciones: hotel.instalaciones.concat(instalacionesInput) })
-        setInstalacionesInput("");
-    }
+  return (
+    <>
+      <br />
+      <div>
+        <Grid container justifyContent="center">
+          <Card>
+            <CardContent>
+              <form onSubmit={handleSubmit}>
+                <label>
+                  <TextField
+                    type="text"
+                    id="nombre"
+                    label="Nombre"
+                    name="nombre"
+                    value={hotel.nombre ?? ""}
+                    onChange={onChange}
+                    required
+                  />
+                </label>
+                <br />
 
-    function validarArrays() {
-        if (instalacionesInput.length === 0) {
-            return false
-        } else {
-            return true
-        }
-    }
+                <label>
+                  <TextField
+                    type="text"
+                    id="imagen"
+                    label="URL de Imagen"
+                    name="imagen"
+                    value={hotel.imagen ?? ""}
+                    onChange={onChange}
+                    required
+                  />
+                </label>
+                <br />
+                <br />
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        if (validarArrays()) {
-            updateHoteles(hotel, hotelID, hotel.ciudadID).then(() => {
-                navigate("/dashboardHoteles");
-            }).catch(e => console.error({ error: e, msg: "ta malardo" }));
-        }
-    }
+                <Typography align="center">
+                  <label>
+                    Ranking:
+                    <Form.Select
+                      name="ranking"
+                      value={hotel.ranking ?? ""}
+                      onChange={onChange}
+                      required
+                    >
+                      <option value="0">0</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </Form.Select>
+                  </label>
+                </Typography>
+                <br />
 
-    const getNombreHab = async (id) => {
-
-        return await queryHabitacion(id)
-    }
-
-
-    if (loading) return (
-        <CircularProgress />
-    )
-
-    return (
-        <>
-            <br />
-            <div>
-                <Grid container justifyContent="center">
-                    <Card>
-                        <CardContent>
-                            <form onSubmit={handleSubmit}>
-                                <label >
-                                    <TextField type="text" id='nombre' label="Nombre" name="nombre" value={hotel.nombre ?? ""} onChange={onChange} required />
-                                </label>
-                                <br />
-
-                                <label >
-                                    <TextField type="text" id='imagen' label="URL de Imagen" name="imagen" value={hotel.imagen ?? ""} onChange={onChange} required />
-                                </label>
-                                <br />
-                                <br />
-
-                                <Typography align="center">
-                                    <label >
-                                        Ranking:
-                                        <Form.Select name="ranking" value={hotel.ranking ?? ""} onChange={onChange} required>
-                                            <option value="0">0</option>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                        </Form.Select>
-                                    </label>
-                                </Typography>
-                                <br />
-
-                                <div className="arrayContainer">
-                                    <label className="arraysInput">
-
-                                        <TextField type="text" id="instalaciones" name="instalaciones" label="Instalaciones" value={instalacionesInput} onChange={(e) => setInstalacionesInput(e.target.value)} />
-                                        <Button onClick={addInstalaciones} type="button"><AddCircle /></Button>
-                                    </label>
-                                    <br />
-                                    {hotel.instalaciones.map((instalacion, index) => (
-                                        <><Grid container justifyContent="space-between">
-                                            <Grid item><span>{instalacion}</span></Grid>
-                                            <Grid item><Button onClick={() => deleteInstalacion(index)} type="button"><Block/></Button>
-                                            </Grid></Grid></>
-                                    ))}
-                                    <br />
-                                </div>
-                                <div className="arrayContainer">
-                                    <label className="arraysInput">
-
-                                        <TextField type="tipoHabitaciones" id='imagen' label="Tipo de Habitaciones" name="tipoHabitaciones" value={tipoHabitacionesInput} onChange={(e) => setTipoHabitacionesInput(e.target.value)} />
-                                        <Button onClick={addTipoHabitaciones} type="button"><AddCircle /></Button>
-                                    </label>
-                                    <br />
-                                    {hotel.tipoHabitaciones.map((tipoHabitacion, index) => (
-                                        <><Grid container justifyContent="space-between">
-                                            <Grid item><span>{getNombreHab(tipoHabitacion.id).nombre}</span></Grid>
-                                            <Grid item><Button onClick={() => deleteTipoHabitacion(index)} type="button"><Block/></Button>
-                                            </Grid>
-                                        </Grid></>
-                                    ))}
-                                    <br />
-                                </div>
-                                <Grid container justifyContent="center" spacing={1}>
-                                    <Grid item>
-                                        <Button variant='contained' onClick={handleSubmit}>Guardar</Button></Grid>
-                                    <Grid item><Button variant='contained' onClick={() => navigate(`/dashboardHoteles`)}>Regresar</Button></Grid>
-                                </Grid>
-                            </form>
-                        </CardContent>
-                    </Card>
+                <div className="arrayContainer">
+                  <label className="arraysInput">
+                    <TextField
+                      type="text"
+                      id="instalaciones"
+                      name="instalaciones"
+                      label="Instalaciones"
+                      value={instalacionesInput}
+                      onChange={(e) => setInstalacionesInput(e.target.value)}
+                    />
+                    <Button onClick={addInstalaciones} type="button">
+                      <AddCircle />
+                    </Button>
+                  </label>
+                  <br />
+                  {hotel.instalaciones.map((instalacion, index) => (
+                    <>
+                      <Grid container justifyContent="space-between">
+                        <Grid item>
+                          <span>{instalacion}</span>
+                        </Grid>
+                        <Grid item>
+                          <Button
+                            onClick={() => deleteInstalacion(index)}
+                            type="button"
+                          >
+                            <Block />
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </>
+                  ))}
+                  <br />
+                </div>
+                <div className="arrayContainer">
+                  <label className="arraysInput">
+                    <TextField
+                      type="tipoHabitaciones"
+                      id="imagen"
+                      label="Tipo de Habitaciones"
+                      name="tipoHabitaciones"
+                      value={tipoHabitacionesInput}
+                      onChange={(e) => setTipoHabitacionesInput(e.target.value)}
+                    />
+                    <Button onClick={addTipoHabitaciones} type="button">
+                      <AddCircle />
+                    </Button>
+                  </label>
+                  <br />
+                  {hotel.tipoHabitaciones.map((tipoHabitacion, index) => (
+                    <>
+                      <Grid container justifyContent="space-between">
+                        <Grid item>
+                          <span>{getNombreHab(tipoHabitacion.id).nombre}</span>
+                        </Grid>
+                        <Grid item>
+                          <Button
+                            onClick={() => deleteTipoHabitacion(index)}
+                            type="button"
+                          ></Button>
+                        </Grid>
+                      </Grid>
+                    </>
+                  ))}
+                  <br />
+                </div>
+                <Grid container justifyContent="center" spacing={1}>
+                  <Grid item>
+                    <Button variant="contained" onClick={handleSubmit}>
+                      Guardar
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      onClick={() => navigate(`/dashboardHoteles`)}
+                    >
+                      Regresar
+                    </Button>
+                  </Grid>
                 </Grid>
-            </div>
-        </>
-
-    )
+              </form>
+            </CardContent>
+          </Card>
+        </Grid>
+      </div>
+    </>
+  );
 }
 
-export default DashboardEditHotelPage; 
+export default DashboardEditHotelPage;
