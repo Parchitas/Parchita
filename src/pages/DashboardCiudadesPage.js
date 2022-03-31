3723bb74a6af5f3f50cd1c621f72d511996df622
Michellelioy <77360526+Michellelioy@users.noreply.github.com>
2022-03-30 21:55:29 -0400

    Dashboard ciudad estilo
    Dashboard ciudad estilo

--- src/pages/DashboardCiudadesPage.js
+++ src/pages/DashboardCiudadesPage.js
@@ -50,27 +50,52 @@ function DashboardCiudadesPage(){
 
     return (
         <>
-        <div className="container">
+        <div className="imagenFondo">
+            <div className="cont">
+                <div className="contenedor">
+                    <table className="tabla">
+                        <tr className="head">
 
-            {loading ? <div>cargando</div> : null}
-            {ciudades.map(({ id, nombre }) => (
-                <>
-                    <div className="data">
-                        <div className="ciudades">{nombre}</div>
-                        <div className="botonesContainer">
-                            <button onClick={() => navigate(`/dashboardCiudades/${id}`)}> Editar </button>
-                            <button onClick={(() => {handleDelete(id)})}> Eliminar </button>
-                        </div>
+                            <th >Nombre</th>
+
+                            <th>Accion</th>
+
+        
+
+                        </tr>
+                        {loading ? <div>cargando</div> : null}
+                        {ciudades.map(({id,nombre})=>(
+
+                            <>
+                                <tr border>
+                                    <td className="Nombre">
+                                        {nombre}
+                                    </td>
+                                    <td>
+                                        <div className="botonesContainer">
+                                            <button className='button' onClick={() => navigate(`/dashboardCiudades/${id}`)}> Editar </button>
+                                            <button className='buttonE' onClick={(() => {handleDelete(id)})}> Eliminar </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            
+                            </>
+                        ))}
+                    </table>
+
+                <div>
+                    
+                    <div className="title">Agregar una nueva ciudad </div>
+                    <form onSubmit={handleSubmit}>
+                        
+                        <button className='button'onClick = {() => navigate(`/dashboardCiudades/create`)}>Submit</button>
+                    </form> 
+                    <br/>
+                    <div>Volver al Dashboard</div>
+                    <button className='button' onClick={() => navigate(`/dashboard`)}>Click!!!</button>
                     </div>
-                </>
-            ))}
-            <div className="title">Agregar una nueva Ciudad AQUI</div>
-            <form onSubmit={handleSubmit}>
-            
-            <button onClick = {() => navigate(`/dashboardCiudades/create`)}>Submit</button>
-        </form> 
-        <div>Aqui para Devolverse al Dashboard</div>
-        <button onClick={() => navigate(`/dashboard`)}>Click!!!</button>
+                </div>
+            </div>    
         </div>
     </>
     );
